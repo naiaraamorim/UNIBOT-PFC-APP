e88d5805409886438d99f5c1a4c14dd42334e91f
naiaraamorim <naiarataianelimaamorim@gmail.com>
2024-01-16 22:12:18 -0300

    Alterando cores e estilos do chat

--- unibot_app/lib/Screans/scream_chatbot.dart
+++ unibot_app/lib/Screans/scream_chatbot.dart
@@ -14,41 +14,44 @@ class ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        // Área de mensagens
-        Flexible(
-          child: ListView.builder(
-            padding: const EdgeInsets.all(8.0),
-            reverse: true,
-            itemCount: _messages.length,
-            itemBuilder: (_, index) => _messages[index],
-          ),
-        ),
-        if (_messages.isEmpty)
-          Container(
-            height: 250,
-            alignment: Alignment.topCenter,
-            child: const Text(
-              'Tire suas dúvidas sobre a instituição',
-              style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w100),
+    return Container(
+      color: Colors.white12,
+      child: Column(
+        children: <Widget>[
+          // Área de mensagens
+          Flexible(
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              reverse: true,
+              itemCount: _messages.length,
+              itemBuilder: (_, index) => _messages[index],
             ),
           ),
-        // Área centralizada de instruções se não houver mensagens
+          if (_messages.isEmpty)
+            Container(
+              height: 250,
+              alignment: Alignment.topCenter,
+              child: const Text(
+                'Tire suas dúvidas sobre a instituição',
+                style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w100),
+              ),
+            ),
+          // Área centralizada de instruções se não houver mensagens
 
-        // Área de entrada de texto
-        const Divider(height: 1.0),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.green[200],
-                border: Border.all(color: Colors.green, width: 2),
-                borderRadius: BorderRadius.circular(8)),
-            child: _buildTextComposer(),
+          // Área de entrada de texto
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white12,
+                  border: Border.all(color: Colors.green),
+                  borderRadius: BorderRadius.circular(8)),
+              child: _buildTextComposer(),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
