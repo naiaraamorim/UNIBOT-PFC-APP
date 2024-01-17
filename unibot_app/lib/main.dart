ddc6363377130f08b3cc548de60e8e0442ebe033
naiaraamorim <naiarataianelimaamorim@gmail.com>
2024-01-16 22:21:11 -0300

    Adicionando menu

--- unibot_app/lib/main.dart
+++ unibot_app/lib/main.dart
@@ -13,9 +13,43 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: const Text('UNIBOT'),
           titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20),
           backgroundColor: Colors.green,
+        ),
+        drawer: Drawer(
+          // Conteúdo do menu lateral
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                ),
+                child: Text(
+                  'Menu Lateral',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Item 1'),
+                onTap: () {
+                  // Adicione aqui a lógica para lidar com o clique em Item 1
+                },
+              ),
+              ListTile(
+                title: const Text('Item 2'),
+                onTap: () {
+                  // Adicione aqui a lógica para lidar com o clique em Item 2
+                },
+              ),
+              // Adicione mais itens do menu conforme necessário
+            ],
+          ),
         ),
         body: const ChatScreen(),
       ),
